22c4988118cf7d572a31411fea00ea4ea40d762e
MichaLaptop <michael.bleher@gmx.de>
2019-10-26 19:47:46 +0200

    Added function: temperature_from_currentCO2

--- temperature_from_currentCO2.py
+++ temperature_from_currentCO2.py
@@ -1,1 +1,3 @@
-
+def temperature_from_currentCO2(currentCO2_concentration):
+    temperature_increase_from_preindustrial_levels=4*0.3*5.35*ln(currentCO2_concentration/278)
+    return temperature_increase_from_preindustrial_levels
