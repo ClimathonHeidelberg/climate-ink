b7567457412be9583a5bf5c47930b33c0cd5e3fc
Beinabih <jonas.massa@iwr.uni-heidelberg.de>
2019-10-26 22:49:58 +0200

    faction

---
+++ faction.py
@@ -0,0 +1,20 @@
+class Faction():
+    def __init__(self, year):
+        self.budget = self.get_budget()
+        self.year = year 
+
+
+    def get_decision(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_budget(self):
+        return 100
+
+    def get_current_state(self):
+
+        rcp_model = 1
+
+        return rcp_model 
