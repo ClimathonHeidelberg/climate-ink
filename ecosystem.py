ccc8c71f419e7cb1849aab282cdedca6e2cc8caa
Beinabih <jonas.massa@iwr.uni-heidelberg.de>
2019-10-27 01:47:39 +0200

    update computation

--- ecosystem.py
+++ ecosystem.py
@@ -9,7 +9,7 @@ class Ecosystem():
         self.model = model
         # self.client = cdsapi.Client()
         self.dict_temp = {'2019': 1.0, '2020': 1.1, '2021':1.2, '2022':1.2, '2023':1.3, '2024':1.5, '2025':1.6, '2026':1.9}
-        self.co2_concentration = self.current_CO2_concentration()
+        self.co2_concentration = self.init_CO2_concentration()
 
     def get_cds_data(self):
         if self.model == 0:
@@ -24,22 +24,19 @@ class Ecosystem():
             self.model = model
         self.get_cds_data()
 
-    def current_CO2_concentration(self):
+    def init_CO2_concentration(self):
         years_diff = int(self.year) - 1970
         print(years_diff)
         return 326.675+1.48262*((years_diff/10)**(2.3))
 
-    def infinitesimal_CO2_output(self):
-        years_diff = int(self.year) - 1970
-        return 3.41*(years_diff/10)**(1.3)
-
     def temp_from_currentCO2(self):
-        temp_increase_from_preindustrial_lvls=4*0.3*5.35*np.log(self.co2_concentration/278)
+        temp_increase_from_preindustrial_lvls=0.3*5.35*np.log(self.co2_concentration/278)
         return temp_increase_from_preindustrial_lvls
 
-    def update_co2(self):
+    def update_co2(self, faction_emission):
         #per year
-        self.co2_concentration += self.infinitesimal_CO2_output()
+        self.co2_concentration += faction_emission
+        print(self.co2_concentration, 'co2 concentration')
 
 
 class Catastrophy():
@@ -52,4 +49,8 @@ class Catastrophy():
         is_happening = np.random.uniform() < proba
         # print(proba, is_happening)
         # return proba, is_happening
+
+    def check_ExtremeWeather(self, temp):
+        proba = 0.1+ temp*0.4/15
+        is_happening = np.randum.uniform() < probas
 
--- game.py
+++ game.py
@@ -7,33 +7,44 @@ class Game():
 
     def __init__(self, init_year, decision_dict):
         self.init_year = init_year
-        self.end_year = 2100
+        self.end_year = 2080
         self.decision_dict = decision_dict
 
-        self.fraction = Faction(init_year)
+        self.faction = Faction(init_year)
         self.ecosystem = Ecosystem(0, init_year) 
     
 
     def play(self):
         for year in range(self.init_year, self.end_year):
-            with open('input.txt') as json_file:
-                data = json.load(json_file)
-                model = data['model']
-            # print(year)
+            # with open('input.txt') as json_file:
+            #     data = json.load(json_file)
+            #     model = data['model']
+
+
+            print(year)
             # model = self.fraction.get_current_state()
-            temp = self.ecosystem.perform_timestep(model, year)
+            # temp = self.ecosystem.perform_timestep(model, year)
 
             # self.ecosystem.update_co2()
             # print(self.ecosystem.current_CO2_concentration())
             # print('temp' ,self.ecosystem.temp_from_currentCO2())
+            
+            self.faction.compute_velocity()
+            faction_emission = self.faction.compute_additional_co2_emission()
+            self.ecosystem.update_co2(faction_emission)
+            print('tem', self.ecosystem.temp_from_currentCO2())
 
-            data = {}
-            data['temp'] = temp
-            data['plot_temp'] = 'plot_temp.png'
-            with open('output.txt', 'w') as outfile:
-                json.dump(data, outfile)
+            if year == 2023:
+                self.faction.update_co2_acc('Kohlekraft', 3)
+                self.faction.update_co2_acc('Traffic', 10)
 
-            sleep(10)
+            # data = {}
+            # data['temp'] = temp
+            # data['plot_temp'] = 'plot_temp.png'
+            # with open('output.txt', 'w') as outfile:
+            #     json.dump(data, outfile)
+
+            # sleep(10)
 
 
 decision_dict_1 = {'powerplant':'yes'}
